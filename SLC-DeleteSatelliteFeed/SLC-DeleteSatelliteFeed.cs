e148b4a8edf959a8900decdbcc37f5266bae80d4
jkockxskyline <jonas.kockx@skyline.be>
2025-08-25 17:27:58 +0200

    Cleanup of linked objects working

--- SLC-DeleteSatelliteFeed/SLC-DeleteSatelliteFeed.cs
+++ SLC-DeleteSatelliteFeed/SLC-DeleteSatelliteFeed.cs
@@ -11,7 +11,11 @@ using Skyline.DataMiner.Utils.MediaOps.Helpers.Relationships;
 using Newtonsoft.Json;
 using DomHelpers.SlcWorkflow;
 using DomHelpers.Satellitefeeds;
+using DomHelpers.SlcRelationships;
 using System.Linq;
+using Skyline.DataMiner.Net.Upload;
+using Skyline.DataMiner.Net.Messages.SLDataGateway;
+using DomIds;
 
 namespace SLCDeleteSatelliteFeed
 {
@@ -27,10 +31,11 @@ namespace SLCDeleteSatelliteFeed
 		private RelationshipsHelper _relationshipsHelper;
 
 
+
 		// DomHelpers
 		private DomHelper _domHelperWorkflows;
 		private DomHelper _domHelperFeeds;
-
+		private DomHelper _domHelperRelationships;
 
 		private IEngine _engine;
 
@@ -106,31 +111,44 @@ namespace SLCDeleteSatelliteFeed
 			try
 			{
 				domExtactedFeedInstance = _domHelperFeeds.DomInstances.Read(extractedFeedFilter).Single<DomInstance>();
+				var extractedFeedInstance = new ExtractedFeedInstance(domExtactedFeedInstance);
+				_engine.GenerateInformation("delete extracted feed");
+				extractedFeedInstance.Delete(_domHelperFeeds);
 			}
 			catch (Exception ex)
 			{
-				throw new Exception($"Failed to get Raw Extracted Satellite Feed instance with ID : {instance.MappingInfo.ExtractedFeedLink.Value}");
+				// nothing
+				// extracted feed must have already been deleted
 			}
-
-
-			_engine.GenerateInformation("extracted feed");
-			// var extractedFeedInstance = new ExtractedFeedInstance(domExtactedFeedInstance);
-			//_engine.GenerateInformation("delete extracted feed");
-			//extractedFeedInstance.Delete(_domHelperFeeds);
 
 			// delete realtionships and the underlying jobs
 			// get relationships for mapped feed
+			var feedObjectType = _relationshipsHelper.GetObjectType("Satellite Feed");
 
-			var links = _relationshipsHelper.GetLinks(new LinkFilter() { ObjectId = instance.ID.Id.ToString() });
-			_engine.GenerateInformation("start count of no of links");
-			_engine.GenerateInformation($"delete links: no of links: {links.Count()}");
-			_engine.GenerateInformation("'start delete links");
-
-			foreach (var link in links)
+			if (feedObjectType == null)
 			{
-				var relatedJob = link.ParentObjectId;
-				_schedulingHelper.DeleteJob(Guid.Parse(relatedJob));
-				link.Delete();
+				_engine.GenerateInformation("Satellite Feed Object Type not found");
+				// nothing
+			}
+			else
+			{
+				// filter in DOM because helper class not working
+				var domRelationshipFilter = DomInstanceExposers.FieldValues.DomInstanceField(DomIds.SlcRelationships.Sections.LinkInfo.ChildObjectType).Equal(feedObjectType.Id).AND(DomInstanceExposers.FieldValues.DomInstanceField(DomIds.SlcRelationships.Sections.LinkInfo.ChildObjectID).Equal(feedId.ToString()));
+				var domRelationshipsInstances = _domHelperRelationships.DomInstances.Read(domRelationshipFilter);
+
+				// not working --> fix from MO team required
+				// var links = _relationshipsHelper.GetLinks(new LinkFilter() { DomObjectTypeId = feedObjectType.Id, ObjectId = instance.ID.Id.ToString() });
+
+				//_engine.GenerateInformation($"delete links: no of links: {domRelationshipsInstances.Count()}");
+
+				foreach (var link in domRelationshipsInstances)
+				{
+					var relationshipInstance = new LinksInstance(link);
+					var relatedJob = relationshipInstance.LinkInfo.ParentObjectID;
+					_schedulingHelper.DeleteJob(Guid.Parse(relatedJob));
+
+					relationshipInstance.Delete(_domHelperRelationships);
+				}
 			}
 
 			// delete the mapped satellite feed instance
@@ -147,6 +165,8 @@ namespace SLCDeleteSatelliteFeed
 			_domHelperWorkflows = new DomHelper(_engine.SendSLNetMessages, SlcWorkflowIds.ModuleId);
 
 			_domHelperFeeds = new DomHelper(_engine.SendSLNetMessages, SatellitefeedsIds.ModuleId);
+
+			_domHelperRelationships = new DomHelper(_engine.SendSLNetMessages, SlcRelationships.ModuleId);
 
 			_profileHelper = new ProfileHelper(_engine.SendSLNetMessages);
 		}
